5b3e64d28abc97c727392c449be6691a79700560
Rupert Rosse Lachenal <81021778+RuPDuD143@users.noreply.github.com>
2026-09-06 14:11:44 +0800

    Update firebase-config.js

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -21,7 +21,7 @@ export const db = getFirestore(app);
 export { signInWithCustomToken };
 
 export const CONFIG = {
-  CHAIN_ID: '1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea25',
+  CHAIN_ID: '1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4',
   RPC_ENDPOINT: 'https://wax.greymass.com',
   CONTRACT_NAME: 'pesolargame1',
   WAX_TOKEN_CONTRACT: 'eosio.token',
